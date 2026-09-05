5916e177d314e44ce3230db0402a806fe768164d
Erbse09 <107766270+Erbse09@users.noreply.github.com>
2026-09-05 21:57:33 +0900

    Add image rights metadata

---
+++ site-image-rights.js
@@ -0,0 +1,12 @@
+(() => {
+  "use strict";
+
+  window.POIGAME_IMAGE_RIGHTS = Object.freeze({
+    "メメントモリ": Object.freeze({
+      kind: "official-game-image",
+      credit: "(c)BANK OF INNOVATION　該当画像の転載・配布等は禁止しております。",
+      source: "https://mememori-game.com/?goto=game",
+      guideline: "https://mememori-game.com/guideline/"
+    })
+  });
+})();
