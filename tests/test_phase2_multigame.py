d69100aea95a32576d4ded918a6bf95e25008128
Erbse09 <107766270+Erbse09@users.noreply.github.com>
2026-09-08 17:05:10 +0900

    Add second current Moppy Evertale offer
    * Track second current Moppy Evertale offer
    
    * Publish second current Moppy Evertale offer
    
    * Record second current Moppy Evertale offer
    
    * Guard current Moppy Evertale pair
    
    * Update Evertale Moppy target expectations
    
    * Test current Moppy Evertale pair

--- tests/test_phase2_multigame.py
+++ tests/test_phase2_multigame.py
@@ -123,7 +123,10 @@ def test_v32_game_known_sources_are_isolated():
 
     evertale=by_game['エバーテイル']['known_urls_by_source']
     assert set(evertale)=={'moppy','hapitas','warau'}
-    assert evertale['moppy']==['https://pc.moppy.jp/ad/detail.php?site_id=158276']
+    assert set(evertale['moppy'])=={
+        'https://pc.moppy.jp/ad/detail.php?site_id=158276',
+        'https://pc.moppy.jp/ad/detail.php?site_id=158275',
+    }
     assert set(evertale['hapitas'])=={
         'https://hapitas.jp/item/detail/itemid/96066',
         'https://hapitas.jp/item/detail/itemid/91344',
